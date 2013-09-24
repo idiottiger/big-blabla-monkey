dd7d9ac2aad3085b8c47869b569a7c6debfb10ce
idiottiger@gmail.com <idiottiger@gmail.com>
2013-09-24 21:59:36 +0800

    add string tools.

---
+++ code/monkey-cpp/string_tools.c
@@ -0,0 +1,16 @@
+/*
+ * string_tools.c
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: idiot2ger
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "string_tools.h"
+
+const char** string_split_string(const char* str, char splite,
+                                 int* resultArraySize) {
+    return NULL ;
+}
---
+++ code/monkey-cpp/string_tools.h
@@ -0,0 +1,16 @@
+/*
+ * string_tools.h
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: idiot2ger
+ */
+
+#ifndef STRING_TOOLS_H_
+#define STRING_TOOLS_H_
+
+const char** string_split_string(const char* str, char splite,
+                                  int* resultArraySize);
+
+
+
+#endif /* STRING_TOOLS_H_ */
